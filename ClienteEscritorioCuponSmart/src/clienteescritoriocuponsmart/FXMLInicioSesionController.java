7255061809bb48261a651b676b2b752d7b502fed
lizethzds <lizethds.1209@hotmail.com>
2023-11-25 19:05:34 -0600

    Pantallas

--- ClienteEscritorioCuponSmart/src/clienteescritoriocuponsmart/FXMLInicioSesionController.java
+++ ClienteEscritorioCuponSmart/src/clienteescritoriocuponsmart/FXMLInicioSesionController.java
@@ -10,7 +10,10 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -20,8 +23,11 @@ public class FXMLInicioSesionController implements Initializable {
     
     @FXML
     private Label label;
-    
     @FXML
+    private TextField tfUsuario;
+    @FXML
+    private PasswordField tfPassword;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -31,5 +37,10 @@ public class FXMLInicioSesionController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void btnInicioSesion(ActionEvent event) {
+        
+    }
     
 }
